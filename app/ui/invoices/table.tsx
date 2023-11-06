0550d5fcbf48432c10782a14505ae9c1d2ef4fd5
mfghir <fatemeghafari77@gmail.com>
2023-11-06 00:41:39 -0800

    done search component

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
 import InvoiceStatus from '@/app/ui/invoices/status';
+
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
