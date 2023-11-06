34d215deee58ccd4f86f4c8c8553469b3bbf786f
mfghir <fatemeghafari77@gmail.com>
2023-11-06 01:54:04 -0800

    add action file for form

---
+++ app/lib/actions.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { sql } from "@vercel/postgres";
+import { z } from "zod";
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+}
