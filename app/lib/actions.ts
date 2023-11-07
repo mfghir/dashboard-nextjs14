3f212fa1d5e94eb36d35e6bdf4749c600fa2b6d7
mfghir <fatemeghafari77@gmail.com>
2023-11-06 22:42:14 -0800

    done form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,9 @@
 
 import { sql } from "@vercel/postgres";
 import { z } from "zod";
+import { revalidatePath } from "next/cache";
+
+import { redirect } from "next/navigation";
 
 const InvoiceSchema = z.object({
   id: z.string(),
@@ -26,4 +29,7 @@ export async function createInvoice(formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
